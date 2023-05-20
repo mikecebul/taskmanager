887ce7ef2cac6017c2c1d068ed94a89821147162
Mikecebul <mikecebul@gmail.com>
2023-05-19 23:55:14 -0400

    Add footer

--- TaskManager.React/src/App.tsx
+++ TaskManager.React/src/App.tsx
@@ -1,6 +1,8 @@
+import { Plus } from "lucide-react";
+
 function App() {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <nav className="bg-teal">
         <div className="flex items-center justify-between px-6 py-4">
           <p className="text-xl font-semibold text-white">TaskManager</p>
@@ -8,8 +10,20 @@ function App() {
         </div>
       </nav>
 
-      <h1 className="text-4xl font-bold">ToDo's</h1>
-    </>
+      <main className="flex-1 px-2">
+        <h1 className="py-4 text-2xl font-bold">ToDo's</h1>
+        <div className="flex">
+          <button className="rounded-full border-2 border-blue text-xl text-blue">
+            <Plus color="#46bcff"></Plus>
+          </button>
+          <p className="pl-2 text-lg font-medium">Create</p>
+        </div>
+      </main>
+
+      <footer className="flex justify-end bg-darker-gray px-2 py-4">
+        <p className="text-sm text-medium-gray">Privacy Policy</p>
+      </footer>
+    </div>
   );
 }
 
